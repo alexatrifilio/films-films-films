b0e699ac4d529f29a1e3b99780e87bdedd14212b
Alexa Trifilio <alexatrifilio@gmail.com>
2022-09-07 21:15:06 -0300

    changed condition to ternary

--- src/components/Layout/MainNav/index.tsx
+++ src/components/Layout/MainNav/index.tsx
@@ -12,38 +12,41 @@ const MainNav: FC<Props> = ({ hideNav }) => {
       <Container>
         <Navbar.Brand>Films Films Films</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {!hideNav && (
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link className="nav-link" to={"/home"}>
-                Home
-              </Link>
-              <Link className="nav-link" to={"/friends:id"}>
-                Buscar Amigos
-              </Link>
-              <NavDropdown title="Usuario" id="basic-nav-dropdown">
-                <Link className="nav-link" to={"/user"}>
-                  Mi Peril
+        {!hideNav ? (
+          <>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link className="nav-link" to={"/home"}>
+                  Home
                 </Link>
-                <NavDropdown.Divider />
-                <Link className="nav-link" to={"/friends"}>
-                  Mis Amigos
+                <Link className="nav-link" to={"/friends:id"}>
+                  Buscar Amigos
                 </Link>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        )}
-        {hideNav && (
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link className="nav-link" to={"/login"}>
-                Ingresar
-              </Link>
-              <Link className="nav-link" to={"/signup"}>
-                Registrarme
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
+                <NavDropdown title="Usuario" id="basic-nav-dropdown">
+                  <Link className="nav-link" to={"/user"}>
+                    Mi Peril
+                  </Link>
+                  <NavDropdown.Divider />
+                  <Link className="nav-link" to={"/friends"}>
+                    Mis Amigos
+                  </Link>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        ) : (
+          <>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link className="nav-link" to={"/login"}>
+                  Ingresar
+                </Link>
+                <Link className="nav-link" to={"/signup"}>
+                  Registrarme
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </>
         )}
       </Container>
     </Navbar>
