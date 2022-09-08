6e5787b0194a64521b6c4126930dedd13c425c97
Alexa Trifilio <alexatrifilio@gmail.com>
2022-09-07 22:58:07 -0300

    form structure in progress, added signuppayload type

--- src/pages/SignUp/Form/index.tsx
+++ src/pages/SignUp/Form/index.tsx
@@ -1,45 +1,50 @@
 import { FC } from "react";
-import { User } from "../../../types";
+import { SignUpPayload } from "../../../types";
 
 type Props = {
-  onSubmit: (formData: User) => void;
+  onSubmit: (formData: SignUpPayload) => void;
 };
 
 const Form: FC<Props> = ({ onSubmit }) => {
   return (
     <form action="">
       <div>
-        <label htmlFor=""> Nombre</label>
-        <input type="text" />
+        <label htmlFor="name"> Nombre</label>
+        <input type="text" name="name" id="name" />
       </div>
       <div>
-        <label htmlFor=""> Apellido</label>
-        <input type="text" />
+        <label htmlFor="last-name"> Apellido</label>
+        <input type="text" name="last-name" id="last-name" />
       </div>
       <div>
-        <label htmlFor="">Correo Electrónico</label>
-        <input type="text" />
+        <label htmlFor="email">Correo Electrónico</label>
+        <input type="email" name="email" id="email" />
       </div>
       <div>
-        <label htmlFor=""> Contraseña </label>
-        <input type="text" />
+        <label htmlFor="password"> Contraseña </label>
+        <input type="password" name="password" id="password" />
       </div>
       <div>
-        <label htmlFor="">Avatar</label>
-        <input type="text" />
+        <label htmlFor="avatar">Avatar</label>
+        <input type="url" name="avatar" id="avatar" />
       </div>
       <div>
-        <label htmlFor="">Fecha de nacimiento</label>
-        <input type="text" />
+        <label htmlFor="birthdate">Fecha de nacimiento</label>
+        <input type="date" name="birthdate" id="birthdate" />
+      </div>
+
+      {/* País y ciudad deberían ser selects*/}
+      <div>
+        <label htmlFor="country">País</label>
+        <input type="text" name="country" id="country" />
       </div>
       <div>
-        <label htmlFor="">Ciudad</label>
-        <input type="text" />
+        <label htmlFor="city">Ciudad</label>
+        <input type="text" name="city" id="city" />
       </div>
-      <div>
-        <label htmlFor="">País</label>
-        <input type="text" />
-      </div>
+      {/* País y ciudad deberían ser selects*/}
+
+      <button>Registrarme!</button>
     </form>
   );
 };
