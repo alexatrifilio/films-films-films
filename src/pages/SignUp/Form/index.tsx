a0ff19c8c0d8a73f0f128c5870b41ff5bcee434a
Alexa Trifilio <alexatrifilio@gmail.com>
2022-09-07 22:24:06 -0300

    general form structure, done

--- src/pages/SignUp/Form/index.tsx
+++ src/pages/SignUp/Form/index.tsx
@@ -1,11 +1,47 @@
 import { FC } from "react";
+import { User } from "../../../types";
 
 type Props = {
-  onSubmit: (formData) => void;
+  onSubmit: (formData: User) => void;
 };
 
-const Form: FC<Props> = () => {
-  return <form action=""></form>;
+const Form: FC<Props> = ({ onSubmit }) => {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor=""> Nombre</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor=""> Apellido</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Correo Electrónico</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor=""> Contraseña </label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Avatar</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Fecha de nacimiento</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Ciudad</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">País</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
 };
 
 export { Form as SignUpForm };
