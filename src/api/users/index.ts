00c3920f61b2788841ce74c7d38f711b679e97e8
Alexa Trifilio <alexatrifilio@gmail.com>
2022-09-18 14:38:26 -0300

    agregué erase y get a usersApi

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -11,4 +11,13 @@ const getAll = async (): Promise<User[]> => {
   return mapToArray(response.data);
 };
 
-export const usersApi = { add, getAll };
+const get = async (id: string): Promise<User> => {
+  const response = await apiDB(`/users/${id}.json`);
+  return response.data;
+};
+
+const erase = async (id: string) => {
+  await apiDB.delete(`/users/${id}.json`);
+};
+
+export const usersApi = { add, getAll, get };
