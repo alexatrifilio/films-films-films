2364cd0865ecd9ef094cc869418ba2b82228702b
Alexa Trifilio <alexatrifilio@gmail.com>
2022-10-24 13:39:54 -0300

    probando try catch, me tira error en el type de get y getAll

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -3,25 +3,45 @@ import { SignUpPayload, User } from "../../types";
 import { apiDB } from "../../utils";
 
 const save = async (user: SignUpPayload) => {
-  await apiDB.post("/users.json", user);
+  try {
+    await apiDB.post("/users.json", user);
+  } catch (err) {
+    console.log(err);
+  }
 };
 const getAll = async (): Promise<User[]> => {
-  const response = await apiDB("/users.json");
-  return mapToArray(response.data);
+  try {
+    const response = await apiDB("/users.json");
+    return mapToArray(response.data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const get = async (id: string): Promise<User> => {
-  const response = await apiDB(`/users/${id}.json`);
-  return response.data;
+  try {
+    const response = await apiDB(`/users/${id}.json`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const erase = async (id: string) => {
-  await apiDB.delete(`/users/${id}.json`);
+  try {
+    await apiDB.delete(`/users/${id}.json`);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const patch = async (id: string, payload: Partial<User>) => {
-  const response = await apiDB.patch(`/users/${id}.json`, payload);
-  return response.data;
+  try {
+    const response = await apiDB.patch(`/users/${id}.json`, payload);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const usersApi = { save, getAll, get, erase, patch };
